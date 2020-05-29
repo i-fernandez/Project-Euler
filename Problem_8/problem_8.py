dd62cf93616f9c0d59636df031385e01e714ea2f
i-fernandez <israel.fernandez.80@gmail.com>
2020-05-29 10:20:38 +0200

    problem 8

--- Problem_8/problem_8.py
+++ Problem_8/problem_8.py
@@ -2,32 +2,44 @@
 Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. 
 What is the value of this product?
 """
-from pathlib import Path
 
-file_name = 'numbers.txt'
-
-numbers = []
-with open(file_name,'r') as f:
-    n = f.read(1)
-    while n:
-        if n != '\n':
-            numbers.append(int(n))
+# Lee el contenido del fichero y lo devuelve como lista
+def read_numbers():
+    numbers = []
+    with open(file_name,'r') as f:
         n = f.read(1)
-
-#print(numbers)
-#print(f'n: {len(numbers)}')
-
-# Solucion chapuzera:
+        while n:
+            if n != '\n':
+                numbers.append(int(n))
+            n = f.read(1)
+    return numbers
 
 # Multiplica los 13 elementos de numbers a partir del elemento start
-def get_multi(start, numbers):
+def get_multi(numbers):
     mult = 1
-    for i in numbers[start:start+13]:
-        print(i)
+    for i in numbers:
         mult = mult * i
     return mult
 
-print(get_multi(0,numbers))
+file_name = 'numbers.txt'
+# Obtiene los numeros del fichero
+numbers = read_numbers()
+maximum = 0
+max_subset = []
+max_i = 0
+for n in range(0,988):
+    subset = numbers[n:n+13]
+    if (0,1) not in subset:
+        # Obtiene la multiplicacion
+        mult = get_multi(subset)
+        if (mult > maximum):
+            maximum = mult
+            max_subset = subset
+            max_i = n
 
-# Solucion un poco menos chapucera
-# Comprueba si hay 0 o 1 en los 13 numeros para no evaluarlos
+
+print(f'Maximum product is {maximum}')
+print(f'Max subset: {max_subset}')
+print(f'Starting index: {max_i}')
+
+

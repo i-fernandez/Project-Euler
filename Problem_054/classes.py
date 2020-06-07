f7f8505df29678657ad0bac1ada30d17d0dd1397
i-fernandez <israel.fernandez.80@gmail.com>
2020-06-07 20:00:00 +0200

    problem 54

--- Problem_054/classes.py
+++ Problem_054/classes.py
@@ -8,8 +8,10 @@ Created on Sun Jun  7 12:25:53 2020
 
 class Card():
     def __init__(self, tuple):
-        self.value = tuple[0:-1]
-        self.suit = tuple[-1]
+        self.value = tuple[0]
+        self.suit = tuple[1]
+        #self.value = tuple[0:-1]
+        #self.suit = tuple[-1]
         #print(f'value: {tuple[0:-1]} suit: {tuple[-1]}')
 
     @property
@@ -17,7 +19,9 @@ class Card():
         return self.__value
     @value.setter
     def value(self, value):
-        if value == 'J':
+        if value == 'T':
+            self.__value = 10
+        elif value == 'J':
             self.__value = 11
         elif value == 'Q':
             self.__value = 12
@@ -80,19 +84,18 @@ class Hand():
         s = 0
         for i in self.cards:
             s += i.value
-        print(f'suma: {suma} s: {s}')
         return (suma == s and self.same_suit())
-        
         
     # All cards are consecutive values of same suit
     def is_straight_flush(self):
         values = self.get_values()
         values.sort()
-        print(f'values: {values}')
         for i in range(0,4):
             if values[i] != values[i+1]-1:
-                return False
-        return self.same_suit()
+                return 0
+        if self.same_suit():
+            return values[4]
+        return 0
     
     # Four cards of the same value
     def is_four_of_a_kind(self):
@@ -100,34 +103,195 @@ class Hand():
         values.sort()
         if values[0] == values[1] and values[1] == values[2] and \
             values[2] == values[3]:
-            #print(f'Poker de {values[0]}')
             return values[0]
         if values[1] == values[2] and values[2] == values[3] and \
             values[3] == values[4]:
-            #print(f'Poker de {values[1]}')
             return values[1]
-        return False
-    
-    def is_full_house():
-        return 'highest'
-    
-    def is_flush():
-        return 'highest'
-    
-    def is_straight():
-        return 'highest'
-    
-    def is_three_of_a_kind():
-        return 'highest'
-    
-    def is_two_pairs():
-        return 'highest'
-    
-    def is_one_pair():
-        return 'highest'
-    
-    def is_high_card():
-        return 'highest'
-    
-    def get_hand():
         return 0
+    
+    # Three of a kind and a pair
+    def is_full_house(self):
+        values = self.get_values()
+        values.sort()
+        if values.count(values[0]) == 3 and values.count(values[4]) == 2:
+            return values[0]
+        if values.count(values[0]) == 2 and values.count(values[4]) == 3:
+            return values[4]
+        return 0
+    
+    # All cards of the same suit
+    def is_flush(self):
+        suits = self.get_suits()
+        values = self.get_values()
+        values.sort(reverse=True)
+        if suits[0] == suits[1] and suits[1] == suits[2] and \
+            suits[2] == suits[3] and suits[3] == suits[4]:
+                return values
+        return 0
+        
+    # All cards are consecutive values
+    def is_straight(self):
+        values = self.get_values()
+        values.sort()
+        for i in range(0,4):
+            if values[i] != values[i+1]-1:
+                return 0
+        return values[4]
+    
+    # Three cards of the same value
+    def is_three_of_a_kind(self):
+        values = self.get_values()
+        for v in values:
+            if values.count(v) == 3:
+                return v
+        return 0
+    
+    # Two different pairs
+    def is_two_pairs(self):
+        values = self.get_values()
+        values.sort(reverse=True)
+        pairs = []
+        for v in values:
+            if values.count(v) == 2 and v not in pairs:
+                pairs.append(v)
+        if len(pairs) == 2:
+            return pairs
+        else:
+            return 0
+    
+    # Two cards of the same value
+    def is_one_pair(self):
+        values = self.get_values()
+        for v in values:
+            if values.count(v) == 2:
+                return v
+        return 0
+    
+    # Highest value card
+    def is_high_card(self):
+        values = self.get_values()
+        values.sort(reverse=True)
+        return values
+
+    # 1: gana jugador 1
+    # 2: gana jugador 2
+    # 0: ninguno tiene esa mano
+    # -1: empate    
+    def compare_royal_flush(self, h2):
+        h2_rf = h2.is_royal_flush()
+        if self.is_royal_flush():
+            if h2_rf:
+                return -1
+            return 1
+        if h2_rf:
+            return 2
+        return 0
+    
+    def compare_straight_flush(self, h2):
+        h1_sf = self.is_straight_flush()
+        h2_sf = h2.is_straight_flush()
+        if h1_sf == 0 and h2_sf == 0:
+            return 0
+        if h1_sf > h2_sf:
+            return 1
+        if h2_sf > h1_sf:
+            return 2
+        return self.compare_high_card(h2)
+            
+    def compare_four_kind(self, h2):
+        h1_fk = self.is_four_of_a_kind()
+        h2_fk = h2.is_four_of_a_kind()
+        if h1_fk == 0 and h2_fk == 0:
+            return 0
+        if h1_fk > h2_fk:
+            return 1
+        if h2_fk > h1_fk:
+            return 2
+        return self.compare_high_card(h2)
+        
+    def compare_full_house(self, h2):
+        h1_fh = self.is_full_house()
+        h2_fh = h2.is_full_house()
+        print(f'h1: {h1_fh}  h2: {h2_fh}')
+        if h1_fh == 0 and h2_fh == 0:
+            return 0
+        if h1_fh > h2_fh:
+            return 1
+        if h1_fh < h2_fh:
+            return 2
+        
+    def compare_flush(self, h2):
+        h1_fl = self.is_flush()
+        h2_fl = h2.is_flush()
+        if h1_fl == 0 and h2_fl == 0:
+            return 0
+        if h1_fl == 0 and h2_fl != 0:
+            return 2
+        if h1_fl != 0 and h2_fl == 0:
+            return 1
+        for i in range(0,len(h1_fl)):
+            if h1_fl[i] > h2_fl[i]:
+                return 1
+            if h1_fl[i] < h2_fl[i]:
+                return 2
+        return -1
+    
+    def compare_straight(self, h2):
+        h1_st = self.is_straight()
+        h2_st = h2.is_straight()
+        if h1_st == 0 and h2_st == 0:
+            return 0
+        if h1_st > h2_st:
+            return 1
+        if h1_st < h2_st:
+            return 2
+        return -1
+    
+    def compare_three_kind(self, h2):
+        h1_tk = self.is_three_of_a_kind()
+        h2_tk = h2.is_three_of_a_kind()
+        if h1_tk == 0 and h2_tk == 0:
+            return 0
+        if h1_tk > h2_tk:
+            return 1
+        if h1_tk < h2_tk:
+            return 2
+        return -1
+        
+    def compare_two_pairs(self, h2):
+        h1_tp = self.is_two_pairs()
+        h2_tp = h2.is_two_pairs()
+        if h1_tp == 0 and h2_tp == 0:
+            return 0
+        if h1_tp == 0:
+            return 2
+        if h2_tp == 0:
+            return 1
+        for i in range(0,len(h1_tp)):
+            if h1_tp[i] > h2_tp[i]:
+                return 1
+            if h1_tp[i] < h2_tp[i]:
+                return 2
+        return self.compare_high_card(h2)
+            
+    def compare_one_pair(self, h2):
+        h1_op = self.is_one_pair()
+        h2_op = h2.is_one_pair()
+        if h1_op == 0 and h2_op == 0:
+            return 0
+        if h1_op > h2_op:
+            return 1
+        if h1_op < h2_op:
+            return 2
+        return self.compare_high_card(h2)
+    
+    def compare_high_card(self, h2):
+        h1_hc = self.is_high_card()
+        h2_hc = h2.is_high_card()
+        for i in range(0,len(h1_hc)):
+            if h1_hc[i] > h2_hc[i]:
+                return 1
+            if h1_hc[i] < h2_hc[i]:
+                return 2
+        return -1
+        
--- Problem_054/problem_54.py
+++ Problem_054/problem_54.py
@@ -21,17 +21,107 @@ Created on Sun Jun  7 12:17:18 2020
 
         
 from classes import *
+       
+# Devuelve el jugador que gana (-1 = empate)
+def compare_hand(h1: Hand, h2: Hand):
+    # Royal Flush
+    rf = h1.compare_royal_flush(h2)
+    if rf != 0:
+        print('royal flush')
+        return rf
+    # Straight Flush
+    sf = h1.compare_straight_flush(h2)
+    if sf != 0:
+        print('straight flush')
+        return sf
+    # Four of a kind
+    fk = h1.compare_four_kind(h2)
+    if fk != 0:
+        print('four of a kind')
+        return fk
+    # Full house
+    fh = h1.compare_full_house(h2)
+    if fh != 0:
+        print('full house')
+        return fh
+    # Flush
+    fl = h1.compare_flush(h2)
+    if fl != 0:
+        print('flush')
+        return fl
+    # Straight
+    st = h1.compare_straight(h2)
+    if st != 0:
+        print('straight')
+        return st
+    # Three of a kind
+    tk = h1.compare_three_kind(h2)
+    if tk != 0:
+        print('three of a kind')
+        return tk
+    # Two pairs
+    tp = h1.compare_two_pairs(h2)
+    if tp != 0:
+        print('two pairs')
+        return tp
+    # One pair
+    op = h1.compare_one_pair(h2)
+    if op != 0:
+        print('one pair')
+        return op
+    # High card
+    print('high card')
+    return h1.compare_high_card(h2)
     
+def read_data(file_name):
+    game = []
+    with open(file_name) as f:
+        line = f.readline()
+        while line:
+            all_cards = line.split(' ')
+            hand = []
+            cards_1 = []
+            cards_2 = []
+            for i in range(0,5):
+                c = Card(all_cards[i])
+                cards_1.append(c)
+            for i in range(5,10):
+                c = Card(all_cards[i])
+                cards_2.append(c)
+            hand.append(Hand(cards_1))
+            hand.append(Hand(cards_2))
+            game.append(hand)
+            line = f.readline()
+    return game
 
-        
 
 suits = ['H', 'S', 'D', 'C']
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-hand1 = Hand([Card('5H'), Card('AS'), Card('5D'), Card('5C'), Card('5S')])
-hand2 = ['2C', '3S', '8S', '8D', 'TD']
-cards_1 = [Card('5H'), Card('5C'), Card('6S'), Card('7S'), Card('KD')]
-hand3 = Hand(cards_1)
-print(hand1)
-print(hand1.is_royal_flush())
-print(hand1.is_straight_flush())
-print(hand1.is_four_of_a_kind())
+
+
+
+
+file_name = 'p054_poker.txt'
+
+win_1 = 0
+win_2 = 0
+tie = 0
+game = read_data(file_name)
+
+
+for hand in game:
+    #print(f'{hand[0]} vs {hand[1]}')
+    r = compare_hand(hand[0],hand[1])
+    #print(f'Winner: {r}')
+    if r == 1:
+        win_1 += 1
+    elif r == 2:
+        win_2 += 1
+    elif r == -1:
+        tie += 1
+        
+print(f'Player 1 wins: {win_1}')
+print(f'Player 2 wins: {win_2}')
+print(f'Ties: {tie}')
+
+    
